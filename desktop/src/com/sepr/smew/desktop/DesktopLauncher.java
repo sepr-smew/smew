ab4f35fd37c56695dc102154f03c96343257ac96
James Harrison <00jamesh@gmail.com>
2015-11-17 12:25:45 +0000

    Initial default libgdx and gradle files

---
+++ desktop/src/com/sepr/smew/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sepr.smew.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sepr.smew.SmewFighter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SmewFighter(), config);
+	}
+}
