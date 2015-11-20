882a16288342f7c5deedcebf4e807d96d1f0a807
Avinash Dwarapu <d.nash.avi@gmail.com>
2015-11-20 17:34:28 +0000

    Rename references to SmewFighter

--- desktop/src/com/sepr/smew/desktop/DesktopLauncher.java
+++ desktop/src/com/sepr/smew/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.sepr.smew.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.sepr.smew.SmewFighter;
+import com.sepr.smew.SmewFighters;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SmewFighter(), config);
+		new LwjglApplication(new SmewFighters(), config);
 	}
 }
