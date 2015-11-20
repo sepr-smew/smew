7c7b3b25761c28806b738e8cf2922fe61247ee0d
Avinash Dwarapu <d.nash.avi@gmail.com>
2015-11-20 17:49:42 +0000

    Add a simple screen system

---
+++ core/src/com/sepr/smew/screens/MainMenuScreen.java
@@ -0,0 +1,54 @@
+package com.sepr.smew.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+import com.sepr.smew.SmewFighters;
+
+public class MainMenuScreen implements Screen {
+
+    final SmewFighters game;
+
+    Texture img;
+
+    public MainMenuScreen(final SmewFighters gam) {
+        game = gam;
+		img  = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(img, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+    }
+}
