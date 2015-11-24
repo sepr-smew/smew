0903a72cdcafa6425773ab6fb53002326767ebdb
Tim Ashton <tja519@york.ac.uk>
2015-11-24 11:00:45 +0000

    changed to 720p

--- desktop/src/com/sepr/smew/desktop/DesktopLauncher.java
+++ desktop/src/com/sepr/smew/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.sepr.smew.SmewFighters;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "SMEW FIGHTERS!!!!";
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new SmewFighters(), config);
 	}
 }
