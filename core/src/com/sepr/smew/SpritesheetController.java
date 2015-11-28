b7584ddf0c9de06b0ac629591272ca4dece749fc
Avinash Dwarapu <d.nash.avi@gmail.com>
2015-11-28 17:24:39 +0000

    Make a simple SpritesheetController

---
+++ core/src/com/sepr/smew/SpritesheetController.java
@@ -0,0 +1,32 @@
+package com.sepr.smew;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class SpritesheetController {
+    private final int               frameRows;
+    private final int               frameCols;
+    private final TextureRegion[][] spriteSheet;
+
+    private TextureRegion currentRegion;
+
+    public SpritesheetController(Texture sheet, int rows, int cols) {
+        // TODO(avinashbot): Throw an error if rows/cols is a stupid value.
+        frameRows = rows;
+        frameCols = cols;
+
+        spriteSheet = TextureRegion.split(
+            sheet, sheet.getWidth()/cols, sheet.getHeight()/rows
+        );
+        currentRegion = spriteSheet[0][0];
+    }
+
+    protected void setSprite(int row, int col) {
+        // TODO(avinashbot): Throw an error if out of range.
+        currentRegion = spriteSheet[row][col];
+    }
+
+    public TextureRegion getCurrentSprite() {
+        return currentRegion;
+    }
+}
