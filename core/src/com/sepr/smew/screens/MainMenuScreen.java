dc1b824ef30536c8c3eacbfd9eb9fce73ef62008
Avinash Dwarapu <d.nash.avi@gmail.com>
2015-11-29 16:03:40 +0000

    Add screen javadocs.

--- core/src/com/sepr/smew/screens/MainMenuScreen.java
+++ core/src/com/sepr/smew/screens/MainMenuScreen.java
@@ -3,6 +3,9 @@ package com.sepr.smew.screens;
 import com.sepr.smew.SmewFighters;
 import com.sepr.smew.actors.LogoActor;
 
+/**
+ * The main menu with a title and a play button and other things.
+ */
 public class MainMenuScreen extends AbstractScreen {
     public MainMenuScreen(final SmewFighters gam) {
         super(gam);
--- core/src/com/sepr/smew/screens/RoundScreen.java
+++ core/src/com/sepr/smew/screens/RoundScreen.java
@@ -2,6 +2,9 @@ package com.sepr.smew.screens;
 
 import com.sepr.smew.SmewFighters;
 
+/**
+ * A level in the game set in a given location.
+ */
 public class RoundScreen extends AbstractScreen {
     public RoundScreen(final SmewFighters gam) {
         super(gam);
